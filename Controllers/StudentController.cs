2492fdd495ca6505f81979ed51d263fbde8931ed
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-12-16 10:42:05 -0800

    create StudentController.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using BdEntityFramework.Data.Repositories;
+using BdEntityFramework.Models;
+using System.Linq;
+
+namespace BdEntityFramework.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class StudentController : ControllerBase
+  {
+    IRepository<Student> _repository = null;
+
+    public StudentController(IRepository<Student> repository)
+    {
+      this._repository = repository;
+    }
+
+    // GET api/values
+    [HttpGet]
+    public ActionResult<IEnumerable<Student>> Get() =>
+      _repository.GetAll().ToList();
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    public ActionResult<Student> Get(int id) =>
+      _repository.GetById(id);
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] Student value)
+    {
+      _repository.Insert(value);
+      _repository.Save();
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Student student)
+    {
+      // Update the student properties.
+      Student modified = _repository.GetById(id);
+      modified.Instrument = student.Instrument;
+      modified.LastName = student.LastName;
+      modified.Name = student.Name;
+
+      // Save the changes.
+      _repository.Update(modified);
+      _repository.Save();
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      Student match = _repository.GetById(id);
+      _repository.Delete(match);
+      _repository.Save();
+    }
+  }
+}
