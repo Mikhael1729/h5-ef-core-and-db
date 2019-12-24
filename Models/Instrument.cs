7d54746e152d97eb34847cc4eec1da2616a925b5
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-12-23 21:58:52 -0800

    use string restrictions in all entities.

--- Models/Instrument.cs
+++ Models/Instrument.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BdEntityFramework.Models
 {
   public class Instrument: Entity
   {
+    [MaxLength(500)]
     public int Name { get; set; }
     public double Price { get; set; }
     public int StudentId { get; set; }
--- Models/Subject.cs
+++ Models/Subject.cs
@@ -1,9 +1,11 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BdEntityFramework.Models
 {
   public class Subject : Entity
   {
+    [MaxLength(500)]
     public string Name { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime EndTime { get; set; }
